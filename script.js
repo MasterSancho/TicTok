f5173726fcdde359f5621e72e97b579d12e197d9
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-02-02 22:28:41 +0200

    stage 9
    input img x and o

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ var table;
 var mat = [[0,0,0],[0,0,0],[0,0,0]];
 var tableSize = 3;
 var not = 'nothing.jpg';
+var x = 'xImg.jpg';
+var o = 'oImg.jpg';
 var size100 = '100px';
 var noth = '<img src=' + not + ' width=' + size100 + ' height=' + size100 + '>';
+var x = '<img src=' + x + ' width=' + size100 + ' height=' + size100 + '>';
+var o = '<img src=' + o + ' width=' + size100 + ' height=' + size100 + '>';
 var textP1 = "<h2>Player One turn</h2>";
 var textP2 = "<h2>Player Two turn</h2>";
 var turn = false;
@@ -13,7 +17,7 @@ function play() {
     let rows = document.getElementById("input1").value
     let cols = document.getElementById("input2").value
         turn = !turn;
-        mat[rows - 1][cols - 1] = turn;
+    mat[rows - 1][cols - 1] = turn ? x : o;
     document.getElementById("textInfo").innerHTML = turn ? textP2 : textP1;
     creatTable()
 }

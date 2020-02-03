56544c469b0a8d0dab3dfd7728081f7a80bea90f
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-02-03 08:07:24 +0200

    stage 11
    1-cheack square if enpty
    2-stepCount
    3-msg wrongSquare
    4-msg draw

--- script.js
+++ script.js
@@ -13,14 +13,23 @@ var textP2 = "<h2>Player Two turn</h2>";
 var turn = false;
 var info = document.getElementById("textInfo");
 var textWrongSquare = "<h2>Choice empty square please</h2>";
+var textPlayAgain = "<h2>Draw Play Again</h2>";
+var stepCount = 0;
 
 function play() {
     let rows = document.getElementById("input1").value
     let cols = document.getElementById("input2").value
-    turn = !turn;
+    if (mat[rows - 1][cols - 1] == noth) {
+        stepCount++;
+        turn = !turn;
         mat[rows - 1][cols - 1] = turn ? x : o;
         document.getElementById("textInfo").innerHTML = turn ? textP2 : textP1;
         creatTable()
+    } else {
+        document.getElementById("textInfo").innerHTML = textWrongSquare;
+    } if (stepCount == 9) {
+        document.getElementById("textInfo").innerHTML = textPlayAgain;
+    }
 }
 function creatTable() {
     table = '<table border=1>';
@@ -36,7 +45,6 @@ function creatTable() {
 }
 
 function ofNothing() {
-    var not = 'nothing.jpg';
     for (let r = 0; r < tableSize; r++) {
         for (let c = 0; c < tableSize; c++) {
             mat[r][c] = noth;
